7c8d2155714dcfeebb9724dd0eb254ca74fda97b
Alex Mc'key <mckey2002@yandex.ru>
2024-12-03 00:37:35 +0300

    Some optimization and reorganization for Day01

--- src/Year2024/Day01/Day01.kt
+++ src/Year2024/Day01/Day01.kt
@@ -7,19 +7,24 @@ import AoCLib.Collections.groupCount
 import kotlin.math.absoluteValue
 
 object Day01 : DayOf2024(1, "Historian Hysteria") {
+    private fun readInput(): Pair<List<Int>,List<Int>> =
+        lines
+            .map { it.split("""\s+""".toRegex())
+                .map{ it.toInt() }
+                .let { it[0] to it[1] } }
+            .unzip()
+
+
     override fun first(): Any? {
-        val ids = lines.map { it.split("""\s+""".toRegex()). map{ it.toInt() } }
-        val ids1 = ids.map{ it.first() }
-        val ids2 = ids.map{ it.last() }
+        val (ids1, ids2) = readInput()
         return ids1.sorted().zip(ids2.sorted())
             .sumOf { (a, b) -> (a - b).absoluteValue }
     }
 
     override fun second(): Any? {
-        val ids = lines.map { it.split("""\s+""".toRegex()). map{ it.toInt() } }
-        val ids1 = ids.map{ it.first() }
-        val ids2 = ids.map{ it.last() }.groupCount()
-        return ids1.sumOf { it * ids2.getOrDefault(it, 0) }
+        val (ids1, ids2) = readInput()
+        val ids2Cnt = ids2.groupingBy { it }.eachCount()
+        return ids1.sumOf { it * ids2Cnt.getOrDefault(it, 0) }
     }
 }
 
