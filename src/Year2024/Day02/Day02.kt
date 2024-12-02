1d7b59360a0f0dfbbefb154acc50603a0044b4ce
Alex Mc'key <mckey2002@yandex.ru>
2024-12-03 00:43:23 +0300

    Some optimization and code reorganization for Day02

--- src/Year2024/Day02/Day02.kt
+++ src/Year2024/Day02/Day02.kt
@@ -8,26 +8,19 @@ import kotlin.math.sign
 
 object Day02 : DayOf2024(2, "Red-Nosed Reports") {
 
+    private fun readInput(): List<Report> = lines.map { Report(it.split(" ").map{ it.toInt() }) }
+
     open class Report(private val report: List<Int>){
-        private val dl = report.zipWithNext(Int::minus)
         private val goalSign = (report.first() - report.last()).sign
-        fun isSafe(): Boolean = dl.all { it.sign == goalSign && it.absoluteValue in 1..3 }
-        fun isSafeWithOneBad(): Boolean {
-            return report.indices.any {
-                Report(report.slice(report.indices - it)).isSafe()
-            }
-        }
+        fun isSafe(): Boolean = report.zipWithNext(Int::minus)
+            .all { it.sign == goalSign && it.absoluteValue in 1..3 }
+        fun isSafeWithOneBad(): Boolean = report.indices
+            .any{ Report(report.slice(report.indices - it)).isSafe() }
     }
 
-    override fun first(): Any? {
-        val reps = lines.map { Report(it.split(" ").map{ it.toInt() }) }
-        return reps.count { it.isSafe() }
-    }
+    override fun first(): Any? = readInput().count { it.isSafe() }
 
-    override fun second(): Any? {
-        val reps = lines.map { Report(it.split(" ").map{ it.toInt() }) }
-        return reps.count { it.isSafe() || it.isSafeWithOneBad() }
-    }
+    override fun second(): Any?  = readInput().count { it.isSafe() || it.isSafeWithOneBad() }
 }
 
 fun main() = SomeDay.mainify(Day02)
